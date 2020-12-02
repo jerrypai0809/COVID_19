cc955bbf6bf35c3a4d20dac56634633f1f392ad5
jerrypai <jerrypai0809@gmail.com>
2020-12-02 15:08:27 +0800

    daily commit

---
+++ daily_run.bat
@@ -0,0 +1,5 @@
+cd C:\Users\admin\Documents\GitHub\COVID_19
+git pull
+C:\Users\admin\anaconda3\python.exe "C:\Users\admin\Documents\GitHub\COVID_19\load_data_gov_hk.py"
+git commit -a -m "daily commit"
+git push
